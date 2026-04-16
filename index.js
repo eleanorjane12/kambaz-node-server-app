9e8c8fee6ea301e8850107ca8e6b9fea41b39677
kikiliu7 <161527266+kikiliu7@users.noreply.github.com>
2026-04-16 16:21:55 -0400

    added quiz dao, model, routes, schema, added to index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ import CourseRoutes from "./kambaz/courses/routes.js";
 import AssignmentsRoutes from "./kambaz/assignments/routes.js";
 import EnrollmentsRoutes from "./kambaz/enrollments/routes.js";
 import ModulesRoutes from "./kambaz/modules/routes.js";
+import QuizRoutes from "./kambaz/quizzes/routes.js";
 const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
 mongoose.connect(CONNECTION_STRING);
 
@@ -36,11 +37,12 @@ if (process.env.SERVER_ENV !== "development") {
 }
 app.use(session(sessionOptions));
 app.use(express.json());
-UserRoutes(app, db);
-CourseRoutes(app, db);
-AssignmentsRoutes(app, db)
-EnrollmentsRoutes(app, db)
-ModulesRoutes(app, db)
+UserRoutes(app)
+CourseRoutes(app)
+AssignmentsRoutes(app)
+EnrollmentsRoutes(app)
+ModulesRoutes(app)
+QuizRoutes(app)
 Lab5(app)
 Hello(app)
 app.listen(process.env.PORT || 4000)
---
+++ kambaz/quizzes/routes.js
@@ -0,0 +1,35 @@
+import QuizzesDao from "./dao";
+
+export default function QuizRoutes(app) {
+  const dao = QuizzesDao();
+
+  app.get("/api/courses/:cid/quizzes", async (req, res) => {
+    const { cid } = req.params;
+    const quizzes = await dao.findQuizzesForCourse(cid);
+    res.json(quizzes);
+  });
+
+  app.post("/api/courses/:cid/quizzes", async (req, res) => {
+    const { cid } = req.params;
+    const quiz = await dao.createQuiz({ ...req.body, course: cid });
+    res.json(quiz);
+  });
+
+  app.put("/api/quizzes/:qid", async (req, res) => {
+    const { qid } = req.params;
+    const status = await dao.updateQuiz(qid, req.body);
+    res.json(status);
+  });
+
+  app.delete("/api/quizzes/:qid", async (req, res) => {
+    const { qid } = req.params;
+    const status = await dao.deleteQuiz(qid);
+    res.json(status);
+  });
+
+  app.get("/api/quizzes/:qid", async (req, res) => {
+    const { qid } = req.params;
+    const quiz = await dao.findQuizById(qid);
+    res.json(quiz);
+  });
+}
