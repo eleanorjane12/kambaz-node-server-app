3fe50bca1ec0e244834e93a29c8186e07fad4c47
eleanorjane12 <collins.eleanor.j@gmail.com>
2026-04-22 20:09:09 -0400

    index fix

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ import QuizRoutes from "./kambaz/quizzes/routes.js";
 import QuestionRoutes from "./kambaz/questions/routes.js";
 import QuizAttemptRoutes from "./kambaz/quiz-attempt/routes.js";
 const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
-mongoose.connect(CONNECTION_STRING).then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+mongoose.connect(CONNECTION_STRING);
 
 
 const app = express()
